ebd133995f46a204a8224c0d16c3aabf71663695
Oscar <oscar870524@gmail.com>
2020-01-08 10:01:57 +0800

    第一版完成

---
+++ Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TV = New System.Windows.Forms.TreeView()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 9)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(127, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Create Test File"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(155, 9)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Read"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TV
+        '
+        Me.TV.Location = New System.Drawing.Point(12, 43)
+        Me.TV.Name = "TV"
+        Me.TV.Size = New System.Drawing.Size(721, 322)
+        Me.TV.TabIndex = 2
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(249, 9)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Save as"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(340, 9)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(113, 23)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Save as (Format)"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(740, 377)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TV)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "JSON Convert"
+        Me.ResumeLayout(False)
+
+    End Sub
+  Friend WithEvents Button1 As System.Windows.Forms.Button
+  Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TV As System.Windows.Forms.TreeView
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,106 @@
+﻿Imports Newtonsoft.Json
+Imports System.IO
+Imports Newtonsoft.Json.Linq
+
+Public Class Form1
+    Dim TEMP_FILE As String = My.Computer.FileSystem.CurrentDirectory() & "\Test.txt"
+    Dim RN As New Random
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim Output As New List(Of DataFormat)
+        For nth = 0 To 10 - 1
+            Dim item As New DataFormat
+            item.Name = nth & "NNNAME"
+            item.Year = RN.Next(11, 30)
+            For n = 0 To RN.Next(11, 30)
+                Dim item_His As New HistoryFormat
+                item_His.Title = n & "HHHHISTORY"
+                item_His.Doing = "asjldfkjas;dlfk"
+                item.History.Add(item_His)
+            Next
+            Output.Add(item)
+        Next
+
+        Dim SW As New StreamWriter(TEMP_FILE)
+        SW.Write(JsonConvert.SerializeObject(Output)) ' , Formatting.Indented 雖然易讀但不能轉回去
+        SW.Close()
+        SW.Dispose()
+
+        MessageBox.Show("Create OK", "Message")
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim SR As New StreamReader(TEMP_FILE)
+        Dim str As String = SR.ReadToEnd
+        SR.Close()
+        SR.Dispose()
+        Dim TN As New TreeNode
+        BuildJsonTree(TN, str)
+        'Dim DataList As List(Of DataFormat) = JsonConvert.DeserializeObject(Of List(Of DataFormat))(str)
+        TV.Nodes.Clear()
+        TV.Nodes.Add(TN)
+    End Sub
+
+    Public Sub BuildJsonTree(ByRef TreeNode As TreeNode, ByVal JsonString As String)
+        If IsJObject(JsonString) Then
+            Dim Jo As JObject = CType(JsonConvert.DeserializeObject(JsonString), JObject)
+            If TreeNode.Text = "" Then
+                TreeNode.Text = "Object"
+            End If
+            TreeNode.Text &= " { " & Jo.Count & " }"
+            For Each item In Jo
+                Dim TN As New TreeNode
+                Select Case item.Value.GetType
+                    Case GetType(JObject), GetType(JArray)
+                        TN = New TreeNode(item.Key)
+                        BuildJsonTree(TN, item.Value.ToString())
+                    Case Else
+                        TN = New TreeNode(item.Key + ":" + item.Value.ToString())
+
+                End Select
+                TreeNode.Nodes.Add(TN)
+            Next
+        ElseIf IsJArray(JsonString) Then
+            Dim JA As JArray = CType(JsonConvert.DeserializeObject(JsonString), JArray)
+            If TreeNode.Text = "" Then
+                TreeNode.Text = "Array"
+            End If
+            TreeNode.Text &= " [ " & JA.Count & " ]"
+            For index = 0 To JA.Count - 1
+                Dim TN As New TreeNode("[ " & index & " ]")
+                BuildJsonTree(TN, JA(index).ToString)
+                TreeNode.Nodes.Add(TN)
+            Next
+        End If
+    End Sub
+
+    Public Function IsJObject(ByVal value As String) As Boolean
+        Try
+            JObject.Parse(value)
+            Return True
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+
+    Public Function IsJArray(ByVal value As String) As Boolean
+        Try
+            JArray.Parse(value)
+            Return True
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+
+End Class
+
+Public Class DataFormat
+    Public Property Name As String
+    Public Property Year As Integer
+    Public Property History As New List(Of HistoryFormat)
+End Class
+
+Public Class HistoryFormat
+    Public Property Title As String
+    Public Property Doing As String
+End Class
